5f358701ac24a91f78285138d55be38c41c4cb1c
ket lolo <moubambaketsia96@gmail.com>
2023-06-17 13:29:34 +0200

    modification apporter sur le code

--- App.py
+++ App.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import LabelEncoder 
+# phase 2 du programme python
+
 
 
 st.write("""
